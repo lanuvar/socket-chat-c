0b3f7b57caa64f0b41b670c44f4e7441767fcc43
iclal <kacariclal@gmail.com>
2026-04-06 00:41:48 +0300

    asenkron yapıya geçildi. çift taraflı mesajlaşma yapılabiliyor

--- tcp_server.c
+++ tcp_server.c
@@ -7,7 +7,6 @@
 
 int main() {
 
-	// 1. TCP için SOCK_STREAM kullanıyoruz.
 	int server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
 	struct sockaddr_in server_address;
@@ -17,32 +16,44 @@ int main() {
 
 	bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address));
 
-	// 2. TCP'de sunucu önce "dinlemeye" başlar. (Aynı anda 5 kişiyi kuyrukta tutabilir)
 	listen(server_socket, 5);
 	printf("TCP Sunucu 8080 portunda baglanti bekliyorrr...\n");
 
 	struct sockaddr_in client_address;
 	socklen_t client_addr_len = sizeof(client_address);
 
-	// 3. Biri kapıyı çalana kadar bekliyoruz accept() fonksiyonu ile
-	// Kapı çalındığında o kişiye özel YENİ bir telefon hattı (client_socket) açıyoruz!!
 	int client_socket = accept(server_socket, (struct sockaddr*)&client_address, &client_addr_len);
 	printf("Biri baglandi! IP: %s\n", inet_ntoa(client_address.sin_addr));
+	printf("Iki yonlu asenkron chat basladi! (Cikmak icin Ctrl+C)\n");
 
-	char buffer[1024];
+	//---BÖLÜNME NOKTASI (Sunucu için)---
+	pid_t process_id = fork();
 
-	// 4. TCP'de hedefi her seferinde belirtmeye gererk yok. Hat zaten özel.
-	// sendto/recvfrom yerine sadece recv ve send kullanıyoruz.
-	int received_bytes = recv(client_socket, buffer, sizeof(buffer), 0);
-
-	if (received_bytes > 0){
-		buffer[received_bytes] = '\0';
-		printf("Gelen mesaj: %s\n", buffer);
-
-		send(client_socket, "TCP Baglantisi Basarili!", 24, 0);
+	if (process_id == 0) {
+		// ÇOCUK SÜREÇ: Sadece istemciden gelen mesajları dinle
+		char buffer[1024];
+		while(1) {
+			int received_bytes = recv(client_socket, buffer, sizeof(buffer), 0);
+			if (received_bytes > 0) {
+				buffer[received_bytes] = '\0';
+				printf("\n[Istemci]: %s", buffer);
+			}
+			else if (received_bytes == 0){
+				printf("\nIstemci baglantiyi kesti.\n");
+				break; //Döngüyü kır ve kapan.
+			}
+		}
+	}
+	else if (process_id > 0) {
+		// PARENT PROCESS: sadece klavyeden yazıp istemciye gönder
+		char message[1024];
+		while(1) {
+			fgets(message, sizeof(message), stdin);
+			send(client_socket, message, strlen(message), 0);
+		}
 	}
 
-	// 5. TCP'de kapatılacak iki hat var:
+
 	close(client_socket); // Client özel hattını kapat
 	close(server_socket); // Dükkanın ana kapısnı kapat
 
